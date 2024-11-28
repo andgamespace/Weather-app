942e3f86f814f7b526693e8951fd7a18fe133f22
andgamespace <anshxc@gmail.com>
2024-11-28 17:53:04 -0600

    weather app beggining to look like a weather app

---
+++ script.js
@@ -0,0 +1,4 @@
+const weatherForm = document.querySelector(".weatherForm");
+const cityInput = document.querySelector("cityInput");
+const card = document.querySelector(".card");
+const apiKey = "9e42e6baf4062faa160963226b6c25c2";
