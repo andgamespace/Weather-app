39c5a2f7cfc4a90fd38d9adcba4b1509154948fe
andgamespace <anshxc@gmail.com>
2024-11-28 19:22:01 -0600

    saving just in case

--- script.js
+++ script.js
@@ -1,4 +1,44 @@
 const weatherForm = document.querySelector(".weatherForm");
-const cityInput = document.querySelector("cityInput");
+const cityInput = document.querySelector(".cityInput");
 const card = document.querySelector(".card");
 const apiKey = "9e42e6baf4062faa160963226b6c25c2";
+
+weatherForm.addEventListener("submit", async event=>{
+    event.preventDefault();
+
+    const city = cityInput.value;
+
+    if(city){
+        try{
+            const weatherData = await getWeatherData(city);
+            displayWeatherInfo(weatherData);
+        }
+        catch(error){
+            console.error(error);
+            displayError(error);
+        }
+    } else{
+        displayError("Please enter a city");
+    }
+});
+async function getWeatherData(city){
+    const apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}`;
+    
+    const response = await fetch(apiUrl);
+
+    console.log(response);
+}
+function displayWeatherInfo(data){
+    
+}
+function getWeatherEmoji(weatherId){
+
+}
+function displayError(message){
+    const errorDisplay = document.createElement("p");
+    errorDisplay.textContent = message;
+    errorDisplay.classList.add("errorDisplay");
+    card.textContent = "";
+    card.style.display = "flex";
+    card.appendChild(errorDisplay);
+}
